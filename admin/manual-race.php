0785c4d78234c2728f61d822202095d8e487b523
Chandler Swift <chandler@chandlerswift.com>
2015-07-23 21:59:37 -0500

    Fixed Manual Race Entry to add correct data type to Database on RS entry

--- admin/manual-race.php
+++ admin/manual-race.php
@@ -2,11 +2,17 @@
 <html>
 <body>
 <?php
+if ($_POST['rs'] = 1){
+	$rs = 1;
+} else {
+	$rs = 0;
+}
+	
 include_once "../includes/db.inc.php";
 	if (isset($_POST["userid"])) {
 		$time_ms = (($_POST['time-min'] * 60000) + ($_POST['time-sec'] * 1000) + $_POST['time-ms']);
 		$timestr = $_POST['time-min'].":".$_POST['time-sec'].".".$_POST['time-ms'];
-		$db->exec("INSERT INTO times (userid,vehicle,time_ms,time_str,timestamp,rs) VALUES ('" . $_POST['userid']. "','" . $_POST['vehicle'] . "','" . $time_ms."','".$timestr."','".$_POST['timestamp']."','".$_POST['rs']."')");
+		$db->exec("INSERT INTO times (userid,vehicle,time_ms,time_str,timestamp,rs) VALUES ('" . $_POST['userid']. "','" . $_POST['vehicle'] . "','" . $time_ms."','".$timestr."','".$_POST['timestamp']."','".$rs."')");
 		echo 'Race Added. <a href="manual-race.php">Add another?</a> | <a href="/">Home</a>';
 	} else { ?>
 <a href="/">Back to Home</a><br><br>
@@ -30,7 +36,7 @@ date and time: <input type="datetime-local" name="timestamp">
   <br>
  Vehicle: <input type="radio" name="vehicle" value="1" checked> Dirt Bike 
   <input type="radio" name="vehicle" value="2"> Go-kart<br>
-  <input type="checkbox" name="rs" value="rs"> Rolling Start?<br>
+  <input type="checkbox" name="rs" value="1"> Rolling Start?<br>
   <input type="submit" value="Submit">
 </form>
 
