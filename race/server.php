b7a5bb22bd2d13903f7b914476411584bc9420f1
Chandler Swift <chandler@chandlerswift.com>
2015-07-26 16:58:07 -0500

    Changed timing on server.php

--- race/server.php
+++ race/server.php
@@ -3,30 +3,32 @@ set_time_limit (600);
 touch('lockfile.tmp');
 
 // setup
-shell_exec('/usr/local/bin/gpio mode 8 out; /usr/local/bin/gpio mode 9 out; /usr/local/bin/gpio mode 7 out');
-shell_exec("/usr/local/bin/gpio write 8 1; /usr/local/bin/gpio write 9 0; /usr/local/bin/gpio write 7 0");
-sleep(1);
+shell_exec('/usr/local/bin/gpio mode 7 out; /usr/local/bin/gpio mode 9 out; /usr/local/bin/gpio mode 8 out'); // Red Green Yellow
+shell_exec("/usr/local/bin/gpio write 7 1; /usr/local/bin/gpio write 9 0; /usr/local/bin/gpio write 8 0");
+sleep(2);
 
 // Blink Yellow Light 3 Times (Ready)
-shell_exec("/usr/local/bin/gpio write 8 0; /usr/local/bin/gpio write 9 1");
+shell_exec("/usr/local/bin/gpio write 7 0; /usr/local/bin/gpio write 9 1");
 usleep(500000);
-shell_exec("/usr/local/bin/gpio write 9 0;");
+shell_exec("/usr/local/bin/gpio write 9 0");
 usleep(500000);
-shell_exec("/usr/local/bin/gpio write 9 1;");
+shell_exec("/usr/local/bin/gpio write 9 1");
 usleep(500000);
-shell_exec("/usr/local/bin/gpio write 9 0;");
+shell_exec("/usr/local/bin/gpio write 9 0");
 usleep(500000);
-shell_exec("/usr/local/bin/gpio write 9 1;");
+shell_exec("/usr/local/bin/gpio write 9 1");
 usleep(500000);
-shell_exec("/usr/local/bin/gpio write 9 0;");
+shell_exec("/usr/local/bin/gpio write 9 0");
 usleep(500000);
+
+echo 
 
 // Turn on Green Light (Go)
-shell_exec("/usr/local/bin/gpio write 8 0; /usr/local/bin/gpio write 9 0; /usr/local/bin/gpio write 7 1");
-sleep(5);
+shell_exec("/usr/local/bin/gpio write 8 1");
+sleep(3);
 
 // Turn all lights off
-shell_exec("/usr/local/bin/gpio write 8 0; /usr/local/bin/gpio write 9 0; /usr/local/bin/gpio write 7 0");
+shell_exec("/usr/local/bin/gpio write 8 0");
 
 echo $milliseconds = round(microtime(true) * 1000);
 unlink('lockfile.tmp');
