aa1b6cd237a5767bb4793b713efbdf85c24f7c49
Chandler Swift <chandler@chandlerswift.com>
2015-08-01 13:51:49 -0500

    dos2unix

--- admin/add-user.php
+++ admin/add-user.php
@@ -2,11 +2,10 @@
 	if (isset($_POST["fullname"])) {
 		include_once "../includes/db.inc.php";
 		$db->exec("INSERT INTO users (fullname) VALUES ('" . $_POST['fullname']. "')");
-		echo 'User Added. <a href="add-user.php">Add another?</a> | <a href="/">Home</a>';
-	} else { ?>
+		echo 'User Added.<br>';
+	} ?>
 <a href="/">Back to Home</a><br><br>
 <form method="POST" action="add-user.php">
 <input type="text" placeholder="First Last" name="fullname">
 <input type="submit" value="Submit">
-</form> 
-<?php } ?>
+</form>
