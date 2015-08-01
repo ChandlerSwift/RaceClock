243ca77c516dd09802cad751a1b38b330d022684
Chandler Swift <chandler@chandlerswift.com>
2015-08-01 12:35:14 -0500

    Added backend

---
+++ race/go.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import json              # Writes out JSON for web client
+import datetime          # Allows collection of accurate times (to microsecond)
+from time import sleep   # Allows waiting
+#import select           # Allows for edge polling--or something
+from os import remove    # Allows file deletion
+from os import path      # Allows checking if file exists
+import wiringpi2         # GPIO
+import sqlite3           # Write to Database when done
+from sys import argv     # Reads Command Line Arguments
+
+def timeToString( time_us ): # something like 123456789 => 2:03.457
+	timeMinutes      = int(time_us / datetime.timedelta(minutes = 1))
+	timeSeconds      = int(time_us / datetime.timedelta(seconds = 1)) - (timeMinutes * 60)
+	timeMilliseconds = int(round(time_us / datetime.timedelta(milliseconds = 1)
+					   - (timeMinutes * 1000 * 60) - (timeSeconds * 1000)))
+	return str(timeMinutes) + ":" + str(timeSeconds).zfill(2) + "." + str(timeMilliseconds).zfill(3)
+
+# Indicate that we're running
+open('lockfile.tmp', 'a').close()
+
+# If for some reason, there's already a stop file there, delete it.
+if path.exists("stop.tmp"):
+	remove("stop.tmp")
+
+# Sets initial JSON
+data = {'status': 'normal', 'status-display': 'Queueing...', 'time': 'asdf'}; 
+with open('status.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+# Set up I/O
+wiringpi2.wiringPiSetup()
+wiringpi2.pinMode(0, 1) # Red Light, Output
+wiringpi2.pinMode(1, 1) # Yellow Light, Output
+wiringpi2.pinMode(2, 1) # Green Light, Output
+wiringpi2.pinMode(3, 0) # Laser Sensor Input
+
+wiringpi2.digitalWrite(0,1)
+sleep(1.5)
+wiringpi2.digitalWrite(0,0)
+sleep(.5)
+
+wiringpi2.digitalWrite(1,1)
+sleep(.5)
+wiringpi2.digitalWrite(1,0)
+sleep(.5)
+wiringpi2.digitalWrite(1,1)
+sleep(.5)
+wiringpi2.digitalWrite(1,0)
+sleep(.5)
+wiringpi2.digitalWrite(1,1)
+sleep(.5)
+wiringpi2.digitalWrite(1,0)
+sleep(.5)
+wiringpi2.digitalWrite(2,1)
+startTime = datetime.datetime.now()
+data['status-display'] = 'Race In Progress...'
+with open('status.json', 'w') as outfile:
+	json.dump(data, outfile)
+sleep(2)
+wiringpi2.digitalWrite(2,0)
+sleep(5)
+
+######### Wait for Laser to detect finish #########
+
+while (not wiringpi2.digitalRead(3)) and (not path.exists("stop.tmp")):
+	sleep(.01)
+
+if path.exists("stop.tmp"):
+	remove("stop.tmp")
+else: # Terminated by lap completion, write to database.
+	endTime = datetime.datetime.now()
+	timeTaken = endTime - startTime
+	time_ms  = int(round(timeTaken / datetime.timedelta(milliseconds=1)))
+	time_str = timeToString(timeTaken)
+	script, userid, vehicle, track = argv
+	conn = sqlite3.connect('../racing.db')
+	c = conn.cursor()
+	c.execute("INSERT INTO times (userid, vehicle, time_ms, time_str) VALUES (?,?,?,?)", (userid, vehicle, time_ms, time_str))
+	conn.commit()
+	conn.close()
+
+	data['status'] = 'done'
+	data['status-display'] = 'Race Complete'
+	data['time'] = str(timeTaken)[:11]
+	with open('status.json', 'w') as outfile:
+		json.dump(data, outfile)
+
+remove('lockfile.tmp')
