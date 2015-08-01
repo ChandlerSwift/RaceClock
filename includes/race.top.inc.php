365b5cbda0feb60d9c85ad6821c4f8dd7b60ad5d
Chandler Swift <chandler@chandlerswift.com>
2015-08-01 12:37:17 -0500

    Semi-final changes, pending testing Part II

---
+++ includes/race.top.inc.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>Race! &middot; Swift Racing</title>
+    <script src="/js/jquery-1.11.1.min.js"></script>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="/css/race.css" rel="stylesheet" type="text/css" />
+    <script src="/js/bootstrap.min.js"></script>
+    <link href="/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <script src="/js/bootstrap-toggle.min.js"></script>
+  </head>
+
+  <body>
+      <div id="nav-wrapper">
+        <ul class="nav nav-tabs">
+          <li role="presentation"><a href="/">Stats</a></li>
+          <li role="presentation"><a href="/admin/">Admin</a></li>
+          <li role="presentation" class="active"><a href="/race/">Race!</a></li>
+        </ul>
+      </div>
+    
+    <div class="container">
